ea81cae4c1127b974642d49b7da88baee4ba6dd0
Owen Goodwin <owen.goodwin@lkqd.com>
2018-06-21 15:26:55 -0700

    add GET and POST request suport to backend and allow CORS.

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,48 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use((req, res, next) => {
-  console.log("first middleware");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
   next();
 });
 
-app.use((req, res, next) => {
-  res.send('Hello from express.');
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: "Post added successfully"
+  });
+});
+
+app.get('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: "fa4357904jg",
+      title: "first server-side post",
+      content: "this is the first server side content."
+    },
+    {
+      id: "gg4357904jg",
+      title: "second server-side post",
+      content: "this is the 2nd server side content."
+    }
+  ];
+  res.status(200).json({
+    message: "posts fetched successfully",
+    posts: posts
+  });
 });
 
 module.exports = app;
